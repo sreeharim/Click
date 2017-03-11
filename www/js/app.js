abc7785bc876889f9d24472004e8e9f6711bff0f
sreeharim <sreeharim1992@gmail.com>
2017-03-11 23:39:33 +0530

    second
    second

---
+++ www/js/app.js
@@ -0,0 +1,46 @@
+var myApp = angular.module("myModule",[])
+				   .controller("myController",function($scope,$timeout){
+				   	$scope.count = 0;
+				   	$scope.addCount = function(){
+				   		
+				   		$scope.count++;
+				   	}
+				   	$scope.counter='Start';
+				   	$scope.onTimeout = function(){
+				   		if($scope.counter == 0){
+				   			$timeout.cancel(mytimeout);
+				   			$scope.counter = 'Play Again';
+				   		}
+				   		else{
+				   			$scope.counter--;
+        					mytimeout = $timeout($scope.onTimeout,1000);
+				   		}
+
+    					}
+    				$scope.start = function(){
+    					if($scope.counter == 'Start'){
+				   			$scope.counter = 10;
+    						mytimeout = $timeout($scope.onTimeout,1000);
+    					}
+    					else if($scope.counter == 'Play Again'){
+				   			$scope.count = -1;
+    						$scope.counter='Start';
+    					}
+    				}	
+    				var mytimeout;
+    				$scope.playSound = function(){
+ 			         var p= window.location.pathname;
+        			var url = p.substring(0, p.lastIndexOf('/')) + "/311.mp3";
+        			 console.log(url);
+        			
+ 			         var myMedia = new Media(url,
+            // success callback
+             function () { console.log("playAudio():Audio Success"); },
+            // error callback
+             function (err) { console.log("playAudio():Audio Error: " + err.code+"  "+ err.message ); }
+    );
+        
+						myMedia.play();     
+    				}
+				   });
+
---
+++ www/js/plugin.media.js
@@ -0,0 +1,14 @@
+window.Media = {
+  get: function(){},
+  play: function(){},
+  stop: function(){},
+  seekTo: function(){},
+  pause: function(){},
+  getDuration: function(){},
+  getCurrentPosition: function(){},
+  startRecord: function(){},
+  stopRecord: function(){},
+  release: function(){},
+  setVolume: function(){},
+  onStatus: function(){}
+};
